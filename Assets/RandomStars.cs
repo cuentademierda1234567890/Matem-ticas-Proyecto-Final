0fcc3db6ac1a24c72f0673707f14d8277d60970b
Yometa777 <49876169+Yometa777@users.noreply.github.com>
2020-06-18 12:33:37 -0400

    Primer Laboratorio
    Este el commit inicial con el cuál empezaré a trabajar sobre el para hacer el laboratorio 2 y 3

---
+++ Assets/RandomStars.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStars : MonoBehaviour
+{
+    public GameObject stars;
+    private Vector3 randomVector;
+    private Renderer renderer;
+    private float Yoffset = 0f;
+    [SerializeField]
+    private float Y_offsetSpeed;
+    public static int numEstrellas = 0;
+    [SerializeField]
+    private int estrellasEnPantalla;
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        Debug.Log(gameObject.transform.localScale.x);
+    }
+
+
+    void Update()
+    {
+        //randomVector = new Vector3(Random.Range(-30,30) , Random.Range(-15,3) , Random.Range(-50,50));
+        randomVector = new Vector3(Random.Range(-13/2f, 13/2f), Random.Range(-5f , 0), Random.Range(-19.5f/2, 19.5f / 2));
+
+        if(numEstrellas < estrellasEnPantalla)
+        {
+            Instantiate(stars, randomVector, transform.rotation);
+            numEstrellas++;
+        }
+
+        renderer.material.mainTextureOffset = new Vector2(0 ,Yoffset += Time.deltaTime * Y_offsetSpeed);
+
+        Debug.Log(numEstrellas);
+
+    }
+
+    
+
+}
---
+++ Assets/StarsMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsMovement : MonoBehaviour
+{
+    private float yMovement;
+    private float timeDestroy = 00f;
+    private int tiempoVida = 0;
+    void Start()
+    {
+        tiempoVida = Random.Range(5, 7);
+        yMovement = Random.Range(-0.1f, -1f) * Time.deltaTime;
+        //yMovement = Random.Range(-0.01f, -0.06f) * Time.deltaTime;
+        var starSize = Random.Range(0.01f, 0.1f);
+        var a = new Vector3(starSize , starSize , starSize);
+        gameObject.transform.localScale = a;
+    }
+
+
+    void Update()
+    {
+        gameObject.transform.Translate(0, yMovement, 0);
+        timeDestroy += Time.deltaTime;
+
+
+        if ((timeDestroy >= tiempoVida) || (gameObject.transform.position.z <= -9.75f))
+        {
+            RandomStars.numEstrellas--;
+            Destroy(this.gameObject);
+        }
+    }
+}

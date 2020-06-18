c8f47b66733250696e7389b8777e131c2d0fcc48
Yometa777 <49876169+Yometa777@users.noreply.github.com>
2020-06-18 16:57:04 -0400

    Laboratorio 3 primera parte sin funcionar
    El laboratorio del primer video de laboratorio 3 pero no se porque a mi no me funciona pese a que copié absolutamente todo el código

---
+++ Assets/Scripts/BezzierFollow.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezzierFollow : MonoBehaviour
+{
+    [SerializeField]
+    public Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector3 NavPosition;
+
+    private float speedModifier;
+
+    private bool coroutineAllowed;
+
+    void Start()
+    {
+        routeToGo = 0;
+        tParam = 0f;
+        coroutineAllowed = true;
+    }
+
+    void Update()
+    {
+        if (coroutineAllowed)
+            StartCoroutine(GoByTheRoute(routeToGo));
+    }
+
+    private IEnumerator GoByTheRoute(int routeNumber)
+    {
+        coroutineAllowed = false;
+
+        Vector3 p0 = routes[routeNumber].GetChild(0).position;
+        Vector3 p1 = routes[routeNumber].GetChild(1).position;
+        Vector3 p2 = routes[routeNumber].GetChild(2).position;
+        Vector3 p3 = routes[routeNumber].GetChild(3).position;
+
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speedModifier;
+
+            NavPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 +
+            3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+
+            transform.LookAt(NavPosition);
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+
+            transform.position = NavPosition;
+            yield return new WaitForEndOfFrame();
+        }
+
+        tParam = 0f;
+
+        routeToGo += 1;
+
+        if (routeToGo > routes.Length - 1)
+            routeToGo = 0;
+
+        coroutineAllowed = true;
+
+    }
+
+}
---
+++ Assets/Scripts/Routes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Routes : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] controlPoints;
+
+    private Vector3 gizmosPosition;
+
+    private void OnDrawGizmos()
+    {
+        for (float t = 0; t <= 1; t += 0.025f)
+        {
+            gizmosPosition = Mathf.Pow(1 - t, 3) * controlPoints[0].position + 3 * Mathf.Pow(1 - t, 2) * t * controlPoints[1].position + 3 * (1 - t) * Mathf.Pow(t, 2) * controlPoints[2].position + Mathf.Pow(t, 3) * controlPoints[3].position;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(gizmosPosition, 0.075f);
+        }
+
+        Gizmos.color = Color.white;
+
+    }
+
+
+}
